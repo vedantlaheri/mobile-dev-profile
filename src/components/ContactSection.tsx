3733cc63aa71a5274842b25c6c19a28f0dc9f1d1
vedantlaheri <66350215+vedantlaheri@users.noreply.github.com>
2025-07-15 10:16:14 +0530

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -108,7 +108,7 @@ const ContactSection = () => {
                   className="border-white text-white hover:bg-white/10 transition-all duration-300 hover:scale-105"
                   asChild
                 >
-                  <a href="/resume.pdf" download>
+                  <a href="/Vedant_Laheri_Resume_main.pdf" download>
                     <Download className="w-5 h-5 mr-2" />
                     Download Resume
                   </a>
